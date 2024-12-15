a90c95c9115f8ba77914b9556839e1befb55f447
vonchange <vonchange@163.com>
2024-12-15 17:39:27 +0800

    油桃官网

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 # !/usr/bin/env bash
-#tar -czvf dist.tar.gz docs
-docker build -t registry.cn-hangzhou.aliyuncs.com/nine/blog:v$1 .
-docker push registry.cn-hangzhou.aliyuncs.com/nine/blog:v$1
+tar -czvf dist.tar.gz docs
+#docker build -t registry.cn-hangzhou.aliyuncs.com/nine/blog:v$1 .
+#docker push registry.cn-hangzhou.aliyuncs.com/nine/blog:v$1
 #scp dist.tar.gz root@$1:/home/app/blog/
 #scp blog.sh root@$1:/home/app/blog/
 #ssh root@$1 bash /home/app/blog/blog.sh
